7e9bc3ae99755c3334b0fc51f584b3b6cd7aa27f
imranisak <imran1701d@gmail.com>
2020-12-24 21:09:00 +0100

    Clinics that are not approved now don't appear in search (duh!)

--- pages/clinics/index.php
+++ pages/clinics/index.php
@@ -26,25 +26,27 @@ $clinics=$databaseConnection->query($SQLselectClinics);
 	<?php require '../../includes/navbar.php'; ?>
 	<?php 
 	foreach($clinics as $clinic){
-		$featuredImage=$clinic['images'];
-		$featuredImage=explode(",", $featuredImage);
-		$featuredImage=$featuredImage[0];
-		$featuredImage=str_replace(['[', ']', '"'], "", $featuredImage);
-		echo "
-		<div class='container'>
-		    <div class='row clinicBox'>
-		        <div class='col-xs-3'><img src='".$featuredImage."' class='featuredImage'></div>
-		        <div class='col-xs-9'>
-		        	<h3 class='clinicNameInBox'>".$clinic['name']."</h3>
-		        	<p class='ownerInBox'>Owner: ".$clinic['owner']."</p>
-		        	<p class='adrressInBox'>Address: ".$clinic['address']."</p>
-		        	<a href='/pages/clinics/clinic.php?ID=".$clinic['ID']."'><button class='btn btn-primary'><span class='glyphicon glyphicon-menu-right' aria-hidden='true'></span>Read more</button></a>
-		        </div>
-		    </div>
-		</div>
-		";
-		/*echo "<h3>".$clinic['name']."</h3><br><p>Owner:".$clinic['owner']."<br>".
-		"Services: ".$clinic['services']."<hr style='width:100%'>";*/
+		if($clinic['approved']){
+			$featuredImage=$clinic['images'];
+			$featuredImage=explode(",", $featuredImage);
+			$featuredImage=$featuredImage[0];
+			$featuredImage=str_replace(['[', ']', '"'], "", $featuredImage);
+			echo "
+			<div class='container'>
+			    <div class='row clinicBox'>
+			        <div class='col-xs-3'><img src='".$featuredImage."' class='featuredImage'></div>
+			        <div class='col-xs-9'>
+			        	<h3 class='clinicNameInBox'>".$clinic['name']."</h3>
+			        	<p class='ownerInBox'>Owner: ".$clinic['owner']."</p>
+			        	<p class='adrressInBox'>Address: ".$clinic['address']."</p>
+			        	<a href='/pages/clinics/clinic.php?ID=".$clinic['ID']."'><button class='btn btn-primary'><span class='glyphicon glyphicon-menu-right' aria-hidden='true'></span>Read more</button></a>
+			        </div>
+			    </div>
+			</div>
+			";
+			/*echo "<h3>".$clinic['name']."</h3><br><p>Owner:".$clinic['owner']."<br>".
+			"Services: ".$clinic['services']."<hr style='width:100%'>";*/
+		}
 	}
 	 ?>
 	<?php require '../../includes/footer.php';
