799fd12f1e83f3f6f056fac51ebd5fe4aa1be8de
Imran Isak <info@imranisak.com>
2021-02-08 14:32:15 +0100

    testing webhooks

--- pages/users/payments/success.php
+++ pages/users/payments/success.php
@@ -1,2 +1,9 @@
 <?php
 echo "yey";
+$data = file_get_contents("php://input");
+$events = json_decode($data, true);
+
+foreach ($events as $event) {
+    // Here, you now have each event and can process them how you like
+    process_event($event);
+}
