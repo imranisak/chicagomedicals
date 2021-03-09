5140788915bd3f1ca509e9e83c2bacc3e1ae8fc9
Imran Isak <info@imranisak.com>
2021-03-09 21:08:07 +0100

    Small fixes

--- pages/clinics/post/deleteClinic.php
+++ pages/clinics/post/deleteClinic.php
@@ -11,15 +11,12 @@ function errorRedirect($message, $databaseConnection, $msg){
 $clinicID=filter_var($_POST['clinicID'], FILTER_SANITIZE_NUMBER_INT);
 if(!$clinicID) errorRedirect("Invalid ID", $databaseConnection, $msg);
 if(!$isLoggedIn) errorRedirect("Must be logged in", $databaseConnection, $msg);
-if(!$isAdmin){
-    $SQLloadClinicForCheck="SELECT ownerID FROM clinics WHERE ID='$clinicID'";
-    $clinicOwnerID=$databaseConnection->query($SQLloadClinicForCheck);
-    if(!$clinicOwnerID) errorRedirect("Error checking ownership", $databaseConnection, $msg);
-    $clinicOwnerID=$clinicOwnerID->fetch_assoc();
-    $clinicOwnerID=$clinicOwnerID['ownerID'];
-    if($clinicOwnerID!=$id) errorRedirect("You can only delete your own clinic...duh!", $databaseConnection, $msg);
-    //errorRedirect("Must be logged in", $databaseConnection, $msg);
-}
+$SQLloadClinicForCheck="SELECT ownerID FROM clinics WHERE ID='$clinicID'";
+$clinicOwnerID=$databaseConnection->query($SQLloadClinicForCheck);
+if(!$clinicOwnerID) errorRedirect("Error checking ownership", $databaseConnection, $msg);
+$clinicOwnerID=$clinicOwnerID->fetch_assoc();
+$clinicOwnerID=$clinicOwnerID['ownerID'];
+if(!$isAdmin) if($clinicOwnerID!=$id) errorRedirect("You can only delete your own clinic...duh!", $databaseConnection, $msg);
 if(!isset($_POST['token']) || !isset($_POST['clinicID'])) errorRedirect("Token or ID missing", $databaseConnection, $msg);
 if($_POST['token']!=$_SESSION['csrf_token']) errorRedirect("Invalid token", $databaseConnection, $msg);
 //Deletes reviews
@@ -46,6 +43,14 @@ $SQLdeleteClinic="DELETE FROM clinics WHERE id='$clinicID'";
 $deletedClinic=$databaseConnection->query($SQLdeleteClinic);
 if(!$deletedClinic) errorRedirect("Error deleting clinic", $databaseConnection, $msg);
 else{
+    $SQLcheckIfUserHasMoreClinic="SELECT * FROM clinics WHERE ownerID='$clinicOwnerID'";
+    $clinics=$databaseConnection->query($SQLcheckIfUserHasMoreClinic);
+    if(!$clinics) $msg->error("Clinic deleted, error checking if user has more clinics.");
+    if($clinics->num_rows==0){
+        $SQLsetUserHasNoClinics="UPDATE users SET hasClinic = '0' WHERE ID='$clinicOwnerID'";
+        $ownerStatusUpdate=$databaseConnection->query($SQLsetUserHasNoClinics);
+        if(!$ownerStatusUpdate) $msg->error("Clinic deleted, but error updating user info!");
+    }
     $databaseConnection->close();
     if($isAdmin) $msg->success("Clinic removed!", "/admin/clinics");
     else $msg->success("Clinic removed!", '/pages/clinics');
