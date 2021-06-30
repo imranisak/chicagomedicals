722f60c4f2af1568b250e48e004ef5d0a2fd5d5c
Imran Isak <info@imranisak.com>
2021-06-30 18:52:09 +0200

    Fixed profile edit bug

--- pages/users/post/editProfile.php
+++ pages/users/post/editProfile.php
@@ -7,7 +7,7 @@ require '../../../includes/sessionInfo.php';
 require '../../../includes/functions.php';
 require '../../../includes/fileUpload.php';
 //exit(var_dump($id));
-$redirectLink="/pages/users/editProfile.php?ID".$id;
+$redirectLink="/pages/users/editProfile.php?ID=".$id;
 if(!isset($_POST['profileToEditID'])) $msg->error("No user ID");
 else $profileToEdit=$_POST['profileToEditID'];
 if($id!=$profileToEdit){
@@ -26,14 +26,24 @@ if(!$user){
 }
 $user=$user->fetch_assoc();
 $currentProfilePicture=$user['profilePicture'];
-if(isset($_POST['name'])) $newname=filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+if(isset($_POST['name'])) {
+    $newname = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    $_SESSION['name']=$newname;
+}
 else $msg->error("Name cannot be empty");
-if(isset($_POST['surname'])) $newsurname=filter_var($_POST['surname'], FILTER_SANITIZE_STRING);
+if(isset($_POST['surname'])) {
+    $newsurname = filter_var($_POST['surname'], FILTER_SANITIZE_STRING);
+    $_SESSION['surname']=$newsurname;
+}
 else $msg->error("Surname cannot be empty.");
-if(isset($_POST['email'])) $newemail=filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+if(isset($_POST['email'])) {
+    $newemail = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $_SESSION['email']=$newemail;
+}
 else $msg->error("Email cannot be empty");
 if (!is_uploaded_file($_FILES['file']['tmp_name'])) $newProfilePicture = $currentProfilePicture;
 else $newProfilePicture = proccessFile($msg, "image");
+$_SESSION['profilePicture']=$newProfilePicture;
 if($msg->hasErrors()) $msg->error("An error has occurred.", "/pages/users/editProfile.php?ID=".$id);
 $SQLupdateUser="UPDATE users SET name='$newname', surname='$newsurname', email='$newemail', profilePicture='$newProfilePicture' WHERE ID='$id'";
 $updateUser=$databaseConnection->query($SQLupdateUser);
@@ -42,12 +52,16 @@ $SQLloadUserClinics="SELECT * FROM clinics WHERE ownerID = '$id'";
 $userClinics=$databaseConnection->query($SQLloadUserClinics);
 if(!$userClinics){
     $databaseConnection->close();
-    $msg->error("Profile updated, but an error happened while updating info of your clinics.", $redirectLink);
+    $msg->error("Profile updated, but an error happened while loading your clinics.", $redirectLink);
 }
 $newClinicOwner=$newname." ".$newsurname;
 foreach ($userClinics as $clinic){
     $SQLupdateClinicOwner="UPDATE clinics SET owner='$newClinicOwner' WHERE ownerID='$id'";
     $update=$databaseConnection->query($SQLupdateClinicOwner);
-    if(!$update) $msg->error("Error updating clinics info.", $redirectLink);
+    if(!$update) {
+        $databaseConnection->close();
+        $msg->error("Error updating clinics info.", $redirectLink);
+    }
 }
+$databaseConnection->close();
 $msg->success("Profile successfully updated!", $redirectLink);
