c4c3cdc95d10c60daa82007986ef2e44024d2ea4
Imran Isak <imran1701d@gmail.com>
2021-09-15 12:33:59 +0200

    Update recaptcha.php

--- includes/recaptcha.php
+++ includes/recaptcha.php
@@ -1,12 +1,12 @@
 <?php
-// recaptcha div - <div class="g-recaptcha" data-sitekey="6LfzjcAZAAAAABoWk_NvnAVnGzhHdJ8xOKIuVYYr"></div>
+// recaptcha div - <div class="g-recaptcha" data-sitekey="nope"></div>
 //if(!isset($msg)) include 'flashMessages.php';
 //$_location=$_SESSION['goBack'];
 //if(isset($_POST['g-recaptcha-response'])){
 //    $captcha=$_POST['g-recaptcha-response'];
 //}
 //if(!$captcha) $msg->error("Please solve the recaptcha!", $_location);
-//$secretKey = "6LfzjcAZAAAAAOmiOiVvOmB_70I7wBpOi94xt7Xb";
+//$secretKey = "na-a";
 //$ip = $_SERVER['REMOTE_ADDR'];
 //$response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
 //$responseKeys = json_decode($response,true);
